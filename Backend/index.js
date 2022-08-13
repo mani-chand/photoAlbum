829980ba1bc48cf3d0d402f582c7b31a4c2bf765
mani-chand <65899331+mani-chand@users.noreply.github.com>
2022-08-13 18:23:19 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({extended:true},{urlencoded:true}))
 const PORT=process.env.PORT | 5000
 app.use('/api/photos',photoRoutes)
-const URL='mongodb+srv://manichand:root@cluster0.oll6q.mongodb.net/photo_album?retryWrites=true&w=majority'
+const URL='mongodb+srv://<uname>:<passwd>@cluster0.oll6q.mongodb.net/photo_album?retryWrites=true&w=majority'
 mongoose.connect(URL)
 .then(res=>{
     app.listen(PORT)
